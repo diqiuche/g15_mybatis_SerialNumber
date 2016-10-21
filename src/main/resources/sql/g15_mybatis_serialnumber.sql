190bec6c34a76a89ebdf95ccd87b2050de305fa5
wangquanjava <wangquanjava@163.com>
2016-10-21 14:43:46 +0800

    sql

---
+++ src/main/resources/sql/g15_mybatis_serialnumber.sql
@@ -0,0 +1,57 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50622
+Source Host           : localhost:3306
+Source Database       : g15_mybatis_serialnumber
+
+Target Server Type    : MYSQL
+Target Server Version : 50622
+File Encoding         : 65001
+
+Date: 2016-10-21 14:43:28
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for serial_table
+-- ----------------------------
+DROP TABLE IF EXISTS `serial_table`;
+CREATE TABLE `serial_table` (
+  `serial_num` varchar(20) NOT NULL,
+  PRIMARY KEY (`serial_num`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of serial_table
+-- ----------------------------
+INSERT INTO `serial_table` VALUES ('00000');
+INSERT INTO `serial_table` VALUES ('00001');
+INSERT INTO `serial_table` VALUES ('00002');
+INSERT INTO `serial_table` VALUES ('00003');
+INSERT INTO `serial_table` VALUES ('00004');
+INSERT INTO `serial_table` VALUES ('00005');
+INSERT INTO `serial_table` VALUES ('00006');
+INSERT INTO `serial_table` VALUES ('00007');
+INSERT INTO `serial_table` VALUES ('00008');
+INSERT INTO `serial_table` VALUES ('00009');
+INSERT INTO `serial_table` VALUES ('00010');
+INSERT INTO `serial_table` VALUES ('00011');
+INSERT INTO `serial_table` VALUES ('00012');
+INSERT INTO `serial_table` VALUES ('00013');
+INSERT INTO `serial_table` VALUES ('00014');
+INSERT INTO `serial_table` VALUES ('00015');
+INSERT INTO `serial_table` VALUES ('00016');
+INSERT INTO `serial_table` VALUES ('00017');
+INSERT INTO `serial_table` VALUES ('00018');
+INSERT INTO `serial_table` VALUES ('00019');
+INSERT INTO `serial_table` VALUES ('00020');
+INSERT INTO `serial_table` VALUES ('00021');
+INSERT INTO `serial_table` VALUES ('00022');
+INSERT INTO `serial_table` VALUES ('00023');
+INSERT INTO `serial_table` VALUES ('00024');
+INSERT INTO `serial_table` VALUES ('00025');
+INSERT INTO `serial_table` VALUES ('00026');
+INSERT INTO `serial_table` VALUES ('00027');
